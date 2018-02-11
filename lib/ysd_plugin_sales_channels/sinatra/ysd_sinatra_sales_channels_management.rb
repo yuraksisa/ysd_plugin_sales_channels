7cda7a414cf8c31374657c70fb9418e7d1124cf7
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2018-02-11 14:04:36 +0100

    channels setup

--- lib/ysd_plugin_sales_channels/sinatra/ysd_sinatra_sales_channels_management.rb
+++ lib/ysd_plugin_sales_channels/sinatra/ysd_sinatra_sales_channels_management.rb
@@ -10,6 +10,12 @@ module YSDPluginSalesChannels
         app.get '/admin/sales-channels/sales-channels/:id/booking-setup', :allowed_usergroups => ['booking_manager','staff'] do
 
           if @sales_channel_booking = ::Yito::Model::SalesChannel::SalesChannelBooking.get(params[:id])
+            if @show_translations = settings.multilanguage_site
+              @tmpl_request = ContentManagerSystem::Template.first({:name => "booking_customer_req_notification.sc.#{@sales_channel_booking.sales_channel.code}"})
+              @tmpl_request_pay_now = ContentManagerSystem::Template.first({:name => "booking_customer_req_pay_now_notification.sc.#{@sales_channel_booking.sales_channel.code}"})
+              @tmpl_confirmation = ContentManagerSystem::Template.first({:name => "booking_customer_notification.sc.#{@sales_channel_booking.sales_channel.code}"})
+              @tmpl_payment_enabled = ContentManagerSystem::Template.first({:name => "booking_customer_notification_payment_enabled.sc.#{@sales_channel_booking.sales_channel.code}"})
+            end
             load_page :sales_channel_booking_configuration
           else
             status 404
